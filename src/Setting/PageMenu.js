2e83b0048393b06706fbe53f81dcccba946a090c
basbev <5806021630070@fitm.kmutnb.ac.th>
2018-09-30 16:39:35 +0700

    editpage keypress

--- src/Setting/PageMenu.js
+++ src/Setting/PageMenu.js
@@ -8,7 +8,8 @@ class Page extends Component {
         this.state = {
             addPage: false,
             pages: [{name:'page 1'}],
-            inputName: ''
+            inputName: '',
+            editPage: null
         }
     }
     componentDidMount() {
@@ -29,26 +30,42 @@ class Page extends Component {
             addPage: true
         })
     }
-    savePage = (e) => {
-        e.preventDefault();
+    savePage = (id) => {
         let tem = this.state.pages
-        tem.push({name: this.state.inputName})
+        if (id === -1) tem.push({name: this.state.inputName})
+        else {
+            tem[id] = {name: this.state.inputName}
+        }
         this.setState({
             pages: tem,
             addPage: false,
-            inputName: ''
+            inputName: '',
+            editPage: null
         })
     }
     handleChange(e) {
         this.setState({ inputName: e.target.value });
     }
+    _handleKeyPress = (id,e) => {
+        console.log(e,id)
+        if (e.key === 'Enter') {
+          //console.log('do validate');
+          this.savePage(id)
+        }
+    }
+    editPage = (id) => {
+        this.setState({ 
+            editPage: id,
+            inputName: this.state.pages[id].name
+        });
+    }
   render() {
     let addPage = <a onClick={this.handleClickAdditem}>+</a>
     if (this.state.addPage) {
         addPage = <div className="input-group addpage">
-            <input type="text" className="form-control addpage text-light border-0  rounded-0 " placeholder="new page" aria-label="new page" onChange={ this.handleChange.bind(this) } aria-describedby="button-addon2"/>
+            <input type="text" className="form-control addpage text-light border-0 rounded-0 " onBlur={()=>this.savePage(-1)} onKeyPress={this._handleKeyPress.bind(this,-1)} placeholder="new page" aria-label="new page" onChange={ this.handleChange.bind(this) } aria-describedby="button-addon2"/>
             <div className="input-group-append">
-              <button className="btn rounded-0 text-light bg-transparent" type="button" id="button-addon2" onClick={this.savePage}>
+              <button className="btn rounded-0 text-light bg-transparent" type="button" id="button-addon2" onClick={()=>this.savePage(-1)}>
                 <i className="fas fa-save"></i>
               </button>
             </div>
@@ -56,11 +73,27 @@ class Page extends Component {
     }
     let listPage = []
     for (let i = 0; i < this.state.pages.length; i++) {
-        listPage.push(<li><a>
-        {
-          this.state.pages[i].name
+        var lspage
+        if (this.state.editPage !== i) {
+            lspage = <li><a>
+            {
+            this.state.pages[i].name
+            }
+            
+                <button className="btn rounded-0 text-light bg-transparent" type="button" id="button-addon2" onClick={()=>this.editPage(i)}>
+                    <i className="fas fa-pen-square"></i>
+                </button>
+                </a>
+            </li>
         }
-        </a></li>)
+        else {
+            lspage = <div className="input-group addpage">
+                <input type="text" className="form-control addpage text-light border-0 rounded-0 " value={this.state.inputName} onBlur={()=>this.savePage(i)} onKeyPress={this._handleKeyPress.bind(this,i)} placeholder="new page" aria-label="new page" onChange={ this.handleChange.bind(this) } aria-describedby="button-addon2" autoFocus/>
+                <div className="input-group-append">
+                </div>
+            </div>
+        }
+        listPage.push(lspage)
     }
     return (
         <div className="wrapper">
