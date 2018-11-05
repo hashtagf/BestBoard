22ece35913358ff57e0f396722773a06b512197b
basbev <5806021630070@fitm.kmutnb.ac.th>
2018-11-05 13:02:42 +0700

    kewinexpertep

--- src/BestBoard/ReactGridLayout.js
+++ src/BestBoard/ReactGridLayout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { WidthProvider, Responsive } from "react-grid-layout"
-import '../../node_modules/react-grid-layout/css/styles.css'
-import '../../node_modules/react-resizable/css/styles.css'
+//import '../../node_modules/react-grid-layout/css/styles.css'
+//import '../../node_modules/react-resizable/css/styles.css'
 import './Main.css'
 import Store from '../store/Store'
 import socketIOClient from 'socket.io-client'
